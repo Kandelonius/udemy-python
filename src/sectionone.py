7d199eba23a842ac6f155c0942afd213f6c51bcc
Shane Kaestner <shane.kaestner@yahoo.com>
2020-07-31 20:30:38 -0700

    initial commit

---
+++ src/sectionone.py
@@ -0,0 +1,17 @@
+age = 30
+
+print(age)
+
+age = 40
+print('age is '+ str(age))
+
+PI = 3.14159
+RADIANS_TO_DEGREES = 180 / PI
+
+_var = 80
+
+float_division = 8/3
+print(float_division)
+
+integer_division = 8//3
+print(integer_division)
